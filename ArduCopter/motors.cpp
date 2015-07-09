9a3af4ccb9734ef288e8ab68c734cc1a669c491c
Paul Riseborough <p_riseborough@live.com.au>
2015-07-09 10:18:25 +1000

    Copter: remove static gyro cal to test new EKF

--- ArduCopter/motors.cpp
+++ ArduCopter/motors.cpp
@@ -164,15 +164,6 @@ bool Copter::init_arm_motors(bool arming_from_gcs)
     calc_distance_and_bearing();
 
     if(did_ground_start == false) {
-        startup_ground(true);
-        // final check that gyros calibrated successfully
-        if (((g.arming_check == ARMING_CHECK_ALL) || (g.arming_check & ARMING_CHECK_INS)) && !ins.gyro_calibrated_ok_all()) {
-            gcs_send_text_P(SEVERITY_HIGH,PSTR("Arm: Gyro calibration failed"));
-            AP_Notify::flags.armed = false;
-            failsafe_enable();
-            in_arm_motors = false;
-            return false;
-        }
         did_ground_start = true;
     }
 
