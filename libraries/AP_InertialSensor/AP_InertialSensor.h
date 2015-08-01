d9b5654aab5250194f4d9dde3e4bf39a903d81f0
Randy Mackay <rmackay9@yahoo.com>
2015-08-01 15:40:40 +0900

    INS: add USE parameters

--- libraries/AP_InertialSensor/AP_InertialSensor.h
+++ libraries/AP_InertialSensor/AP_InertialSensor.h
@@ -88,6 +88,9 @@ public:
     uint8_t register_gyro(void);
     uint8_t register_accel(void);
 
+    /// use this accel and gyro instance for attitude, velocity and position estimates
+    bool use(uint8_t instance);
+
 #if !defined( __AVR_ATmega1280__ )
     // perform accelerometer calibration including providing user instructions
     // and feedback
@@ -146,12 +149,14 @@ public:
     uint8_t get_gyro_count(void) const { return _gyro_count; }
     bool gyro_calibrated_ok(uint8_t instance) const { return _gyro_cal_ok[instance]; }
     bool gyro_calibrated_ok_all() const;
+    bool use_gyro(uint8_t instance) const;
 
     bool get_accel_health(uint8_t instance) const { return (instance<_accel_count) ? _accel_healthy[instance] : false; }
     bool get_accel_health(void) const { return get_accel_health(_primary_accel); }
     bool get_accel_health_all(void) const;
     uint8_t get_accel_count(void) const { return _accel_count; };
     bool accel_calibrated_ok_all() const;
+    bool use_accel(uint8_t instance) const;
 
     // get accel offsets in m/s/s
     const Vector3f &get_accel_offsets(uint8_t i) const { return _accel_offset[i]; }
@@ -307,6 +312,11 @@ private:
     // filtering frequency (0 means default)
     AP_Int8     _accel_filter_cutoff;
     AP_Int8     _gyro_filter_cutoff;
+
+#if INS_MAX_INSTANCES > 1
+    // use for attitude, velocity, position estimates
+    AP_Int8     _use[INS_MAX_INSTANCES];
+#endif
 
     // board orientation from AHRS
     enum Rotation _board_orientation;
